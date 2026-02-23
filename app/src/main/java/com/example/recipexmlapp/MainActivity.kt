ea75e258d1c6c759ba39625ae56ca4949b24a45e
kamgi <kamgidayatov@gmail.com>
2026-02-23 16:25:09 +0300

    RA-03: Fix buttons, implemented new fonts, add linear layout margins, added dimens and add new colors in colors.xml

--- app/src/main/java/com/example/recipexmlapp/MainActivity.kt
+++ app/src/main/java/com/example/recipexmlapp/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.recipexmlapp.databinding.ActivityMainBinding
 
-
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
