4f225e9ecf511153fcfa56d0f286fd5c130881ff
kamgi <kamgidayatov@gmail.com>
2026-02-24 12:05:28 +0300

    RA-03-createButtons: Added new theme in AnroidManifest, add new dimens

--- app/src/main/java/com/example/recipexmlapp/MainActivity.kt
+++ app/src/main/java/com/example/recipexmlapp/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
